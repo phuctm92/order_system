9f7fdf1666322fdd5736bb0d25498a6ef86fa3ae
Phuc Thai <phuc.tm4192@gmail.com>
2021-04-09 14:14:32 +0700

    Add Bootstrap and custom layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,10 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  # :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable, :rememberable, :validatable
 
   def full_name
     "#{first_name} #{last_name}"
